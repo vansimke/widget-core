e64806498b40c3610267a92b7bcff4287800c2b5
Kitson Kelly <me@kitsonkelly.com>
2016-09-07 14:08:09 -0700

    Add getProjector to lang and refactoring and cleanup
    Plus 100% coverage on unit tests

--- src/util/lang.ts
+++ src/util/lang.ts
@@ -1,121 +1,34 @@
+/**
+ * This module contains functional enhancements to the widgets package which are used in more
+ * than one module.
+ */
+
 import { List } from 'immutable';
 import { Handle } from 'dojo-core/interfaces';
 import { Parent, Child, ChildrenMap } from '../mixins/interfaces';
+import { Projector, isProjector } from '../projector';
 
+/**
+ * The valid values for referring to a position for an insertion in a list/array
+ */
 export type Position = number | 'first' | 'last' | 'before' | 'after';
 
-function getIndex<T>(list: List<T> | T[], item: T, position: Position, reference?: T): number {
-	let idx: number;
-	if (typeof position === 'number') {
-		idx = position;
-		const size = Array.isArray(list) ? list.length : list.size;
-		if (idx < 0 || idx > size) {
-			throw new Error('position is out of range');
-		}
-	}
-	else {
-		switch (position) {
-		case 'first':
-			idx = 0;
-			break;
-		case 'last':
-			idx = Array.isArray(list) ? list.length : list.size;
-			break;
-		case 'before':
-			idx = list.indexOf(reference);
-			if (idx === -1) {
-				throw new Error('reference not contained in this list');
-			}
-			break;
-		case 'after':
-			idx = list.indexOf(reference) + 1;
-			if (idx === 0) {
-				throw new Error('reference not contained in this list');
-			}
-			break;
-		default:
-			throw Error(`Invalid position "${position}"`);
-		}
-	}
-	return idx;
-}
-
-export function insertInList<T>(list: List<T>, item: T, position: Position, reference?: T): List<T> {
-	return list.insert(getIndex(list, item, position, reference), item);
-}
-
-export function insertInArray<T>(array: T[], item: T, position: Position, reference?: T): T[] {
-	array.splice(getIndex(array, item, position, reference), 0, item);
-	return array;
-}
-
-function valueReplacer(key: string, value: any): any {
-	if (value instanceof RegExp) {
-		return (`__RegExp(${value.toString()})`);
-	}
-	return value;
-}
-
-function valueReviver(key: string, value: any): any {
-	if (value.toString().indexOf('__RegExp(') === 0) {
-		const [ , regExpStr ] = value.match(/__RegExp\(([^\)]*)\)/);
-		const [ , regExp, flags ] = regExpStr.match(/^\/(.*?)\/([gimy]*)$/);
-		return new RegExp(regExp, flags);
-	}
-	return value;
-}
-
 /**
- * Internal function to convert a state value to a string
- * @param value The value to be converted
+ * A function that resolves the Projector for a given Child.
+ *
+ * @param child The child that is the subject of the function
+ * @return The projector of the child (or undefined)
  */
-export function valueToString(value: any): string {
-	return value
-		? Array.isArray(value) || typeof value === 'object'
-			? JSON.stringify(value, valueReplacer) : String(value)
-		: value === 0
-			? '0' : value === false
-				? 'false' : '';
-}
-
-/**
- * Internal function to convert a string to the likely more complex value stored in
- * state
- * @param str The string to convert to a state value
- */
-export function stringToValue(str: string): any {
-	try {
-		const value = JSON.parse(str, valueReviver);
-		return value;
+export function getProjector(child: Child): Projector | undefined {
+	while (child && child.parent) {
+		child = <Child> <any> child.parent;
 	}
-	catch (e) {
-		if (/^(\-|\+)?([0-9]+(\.[0-9]+)?|Infinity)$/.test(str)) {
-			return Number(str);
-		}
-		if (str) {
-			return str;
-		}
-		return undefined;
-	}
-}
-
-/**
- * A type guard that deterimines if a value is an immutable List or not
- */
-export function isList<T>(value: any): value is List<T> {
-	return value instanceof List;
-}
-
-/**
- * A type guard that checks to see if the value is a Child
- * @param value the value to guard for
- */
-export function isChild<C extends Child>(value: any): value is C {
-	return value && typeof value === 'object' && typeof value.render === 'function';
+	return isProjector(child) && child;
 }
 
 /**
  * A utility function that generates a handle that destroys any children
+ *
  * @param parent The parent that the handle relates to
  * @param child The child (or array of children) that the handle relates to
  */
@@ -176,5 +89,162 @@ export function getRemoveHandle<C extends Child>(parent: Parent, child: C | C[] 
 			}
 		};
 	}
+}
 
+/**
+ * An internal function that resolves the index in a list or an array when provided with a
+ * position reference
+ *
+ * @param list The list or array that the index should be determined for
+ * @param position The posistion in the list or array
+ * @param reference If relative posistion (`before` or `after`) the item that the index is relative to
+ */
+function getIndex<T>(list: List<T> | T[], position: Position, reference?: T): number {
+	let idx: number;
+	if (typeof position === 'number') {
+		idx = position;
+		const size = Array.isArray(list) ? list.length : list.size;
+		if (idx < 0 || idx > size) {
+			throw new Error('position is out of range');
+		}
+	}
+	else {
+		switch (position) {
+		case 'first':
+			idx = 0;
+			break;
+		case 'last':
+			idx = Array.isArray(list) ? list.length : list.size;
+			break;
+		case 'before':
+			idx = list.indexOf(reference);
+			if (idx === -1) {
+				throw new Error('reference not contained in this list');
+			}
+			break;
+		case 'after':
+			idx = list.indexOf(reference) + 1;
+			if (idx === 0) {
+				throw new Error('reference not contained in this list');
+			}
+			break;
+		default:
+			throw Error(`Invalid position "${position}"`);
+		}
+	}
+	return idx;
+}
+
+/**
+ * Insert an item into an array at the provided posistion
+ *
+ * **NOTE**: This does mutate the passed array
+ *
+ * @param array The array that is the source
+ * @param item The item to insert
+ * @param position The position in the array to insert the item
+ * @param reference If the position is a relative position ('before' or 'after') then the reference
+ *                  that that posistion refers to
+ * @return The original array with the item inserted
+ */
+export function insertInArray<T>(array: T[], item: T, position: Position, reference?: T): T[] {
+	array.splice(getIndex(array, position, reference), 0, item);
+	return array;
+}
+
+/**
+ * Insert an item into a immutable List at the provided posistion
+ *
+ * @param list The list that is the source
+ * @param item The item to insert
+ * @param position The position in the list to insert the item
+ * @param reference If the position is a relative position ('before' or 'after') then the reference
+ *                  that that posistion refers to
+ * @return A new list with the inserted
+ */
+export function insertInList<T>(list: List<T>, item: T, position: Position, reference?: T): List<T> {
+	return list.insert(getIndex(list, position, reference), item);
+}
+
+/**
+ * A type guard that checks to see if the value is a Child
+ *
+ * @param value the value to guard for
+ */
+export function isChild<C extends Child>(value: any): value is C {
+	return value && typeof value === 'object' && typeof value.render === 'function';
+}
+
+/**
+ * A type guard that deterimines if a value is an immutable List or not
+ *
+ * @param value The value to guard for
+ */
+export function isList<T>(value: any): value is List<T> {
+	return value instanceof List;
+}
+
+/**
+ * Internal function which converts serialised values of certain objects back into
+ * their native JavaScript representations
+ *
+ * @param key The key that needs to be revived
+ * @param value The value which may contain a special string which represents a serialised object
+ */
+function valueReviver(key: string, value: any): any {
+	if (value.toString().indexOf('__RegExp(') === 0) {
+		const [ , regExpStr ] = value.match(/__RegExp\(([^\)]*)\)/);
+		const [ , regExp, flags ] = regExpStr.match(/^\/(.*?)\/([gimy]*)$/);
+		return new RegExp(regExp, flags);
+	}
+	return value;
+}
+
+/**
+ * Function to convert a string to the likely more complex value stored in state
+ *
+ * @param str The string to convert to a state value
+ */
+export function stringToValue(str: string): any {
+	try {
+		const value = JSON.parse(str, valueReviver);
+		return value;
+	}
+	catch (e) {
+		if (/^(\-|\+)?([0-9]+(\.[0-9]+)?|Infinity)$/.test(str)) {
+			return Number(str);
+		}
+		if (str) {
+			return str;
+		}
+		return undefined;
+	}
+}
+
+/**
+ * Internal function which converts certain values into serialised strings which can
+ * be revived back to their original JavaScript value.
+ *
+ * @param key The name of the key
+ * @param value The value to be serialised
+ */
+function valueReplacer(key: string, value: any): any {
+	if (value instanceof RegExp) {
+		return (`__RegExp(${value.toString()})`);
+	}
+	return value;
+}
+
+/**
+ * Function to convert a state value to a string
+ *
+ * @param value The value to be converted
+ */
+export function valueToString(value: any): string {
+	return value
+		? Array.isArray(value) || typeof value === 'object'
+			? JSON.stringify(value, valueReplacer) : String(value)
+		: value === 0
+			? '0' : value === false
+				? 'false' : '';
 }
--- tests/unit/util/lang.ts
+++ tests/unit/util/lang.ts
@@ -1,96 +1,25 @@
 import * as registerSuite from 'intern!object';
 import * as assert from 'intern/chai!assert';
-import { insertInList, insertInArray, stringToValue, valueToString, isList, isChild } from 'src/util/lang';
-import createRenderable from 'src/mixins/createRenderable';
+import { Handle } from 'dojo-core/interfaces';
+import Promise from 'dojo-shim/Promise';
 import { List } from 'immutable';
+import { VNode, Projector as MaquetteProjector } from 'maquette';
+import createRenderable from '../../../src/mixins/createRenderable';
+import { Projector, ProjectorState } from '../../../src/projector';
+import {
+	insertInList,
+	insertInArray,
+	stringToValue,
+	valueToString,
+	isList,
+	isChild,
+	getRemoveHandle,
+	getProjector
+} from '../../../src/util/lang';
 
 registerSuite({
 	name: 'util/lang',
-	'insertInList()': {
-		'position "before"'() {
-			const first = {};
-			const last = {};
-			const list = List([ first, last ]);
-			const item = {};
-			const result = insertInList(list, item, 'before', last);
-			assert.strictEqual(result.size, 3);
-			assert.strictEqual(result.get(0), first);
-			assert.strictEqual(result.get(1), item);
-			assert.strictEqual(result.get(2), last);
-		},
-		'position "after"'() {
-			const first = {};
-			const last = {};
-			const list = List([ first, last ]);
-			const item = {};
-			const result = insertInList(list, item, 'after', first);
-			assert.strictEqual(result.size, 3);
-			assert.strictEqual(result.get(0), first);
-			assert.strictEqual(result.get(1), item);
-			assert.strictEqual(result.get(2), last);
-		},
-		'position "last"'() {
-			const first = {};
-			const last = {};
-			const list = List([ first, last ]);
-			const item = {};
-			const result = insertInList(list, item, 'last');
-			assert.strictEqual(result.size, 3);
-			assert.strictEqual(result.get(0), first);
-			assert.strictEqual(result.get(1), last);
-			assert.strictEqual(result.get(2), item);
-		},
-		'position "first"'() {
-			const first = {};
-			const last = {};
-			const list = List([ first, last ]);
-			const item = {};
-			const result = insertInList(list, item, 'first');
-			assert.strictEqual(result.size, 3);
-			assert.strictEqual(result.get(0), item);
-			assert.strictEqual(result.get(1), first);
-			assert.strictEqual(result.get(2), last);
-		},
-		'position number'() {
-			const first = {};
-			const last = {};
-			const list = List([ first, last ]);
-			const item = {};
-			const result = insertInList(list, item, 1);
-			assert.strictEqual(result.size, 3);
-			assert.strictEqual(result.get(0), first);
-			assert.strictEqual(result.get(1), item);
-			assert.strictEqual(result.get(2), last);
-		},
-		'throws': {
-			'invalid position'() {
-				assert.throws(() => {
-					insertInList(List(), {}, <any> undefined);
-				}, Error);
-			},
-			'invalid before reference'() {
-				assert.throws(() => {
-					insertInList(List(), {}, 'before', {});
-				}, Error);
-			},
-			'invalid after reference'() {
-				assert.throws(() => {
-					insertInList(List(), {}, 'after', {});
-				}, Error);
-			},
-			'invalid number position'() {
-				assert.throws(() => {
-					insertInList(List(), {}, -1);
-				}, Error);
-				assert.throws(() => {
-					insertInList(List(), {}, 2);
-				}, Error);
-				assert.throws(() => {
-					insertInList(List(), {}, Infinity);
-				}, Error);
-			}
-		}
-	},
+
 	'insertInArray()': {
 		'position "before"'() {
 			const first = {};
@@ -181,6 +110,201 @@ registerSuite({
 			}
 		}
 	},
+
+	'insertInList()': {
+		'position "before"'() {
+			const first = {};
+			const last = {};
+			const list = List([ first, last ]);
+			const item = {};
+			const result = insertInList(list, item, 'before', last);
+			assert.strictEqual(result.size, 3);
+			assert.strictEqual(result.get(0), first);
+			assert.strictEqual(result.get(1), item);
+			assert.strictEqual(result.get(2), last);
+		},
+		'position "after"'() {
+			const first = {};
+			const last = {};
+			const list = List([ first, last ]);
+			const item = {};
+			const result = insertInList(list, item, 'after', first);
+			assert.strictEqual(result.size, 3);
+			assert.strictEqual(result.get(0), first);
+			assert.strictEqual(result.get(1), item);
+			assert.strictEqual(result.get(2), last);
+		},
+		'position "last"'() {
+			const first = {};
+			const last = {};
+			const list = List([ first, last ]);
+			const item = {};
+			const result = insertInList(list, item, 'last');
+			assert.strictEqual(result.size, 3);
+			assert.strictEqual(result.get(0), first);
+			assert.strictEqual(result.get(1), last);
+			assert.strictEqual(result.get(2), item);
+		},
+		'position "first"'() {
+			const first = {};
+			const last = {};
+			const list = List([ first, last ]);
+			const item = {};
+			const result = insertInList(list, item, 'first');
+			assert.strictEqual(result.size, 3);
+			assert.strictEqual(result.get(0), item);
+			assert.strictEqual(result.get(1), first);
+			assert.strictEqual(result.get(2), last);
+		},
+		'position number'() {
+			const first = {};
+			const last = {};
+			const list = List([ first, last ]);
+			const item = {};
+			const result = insertInList(list, item, 1);
+			assert.strictEqual(result.size, 3);
+			assert.strictEqual(result.get(0), first);
+			assert.strictEqual(result.get(1), item);
+			assert.strictEqual(result.get(2), last);
+		},
+		'throws': {
+			'invalid position'() {
+				assert.throws(() => {
+					insertInList(List(), {}, <any> undefined);
+				}, Error);
+			},
+			'invalid before reference'() {
+				assert.throws(() => {
+					insertInList(List(), {}, 'before', {});
+				}, Error);
+			},
+			'invalid after reference'() {
+				assert.throws(() => {
+					insertInList(List(), {}, 'after', {});
+				}, Error);
+			},
+			'invalid number position'() {
+				assert.throws(() => {
+					insertInList(List(), {}, -1);
+				}, Error);
+				assert.throws(() => {
+					insertInList(List(), {}, 2);
+				}, Error);
+				assert.throws(() => {
+					insertInList(List(), {}, Infinity);
+				}, Error);
+			}
+		}
+	},
+
+	'getProjector()'() {
+		const projectorShim: Projector = {
+			projector: <MaquetteProjector> { scheduleRender() {} },
+			emit() { },
+			on(): Handle { return; },
+			own(): Handle { return; },
+			destroy(): Promise<boolean> { return; },
+			listeners: {},
+			children: <List<any>> null,
+			append(): Handle { return; },
+			clear() {},
+			insert(): Handle { return; },
+			getNodeAttributes(): any { return; },
+			render(): VNode { return; },
+			attach(): Promise<Handle> { return; },
+			invalidate(): void {},
+			setRoot(): void {},
+			root: null,
+			document: <Document> null,
+			state: <ProjectorState> null
+		};
+		const projector = getProjector({
+			parent: { children: <List<any>> null, append(): Handle { return; }, parent: projectorShim },
+			own(): Handle { return; },
+			destroy(): Promise<boolean> { return; },
+			render(): VNode { return; },
+			tagName: ''
+		});
+
+		assert.strictEqual(projector, projectorShim);
+	},
+
+	'getRemoveHandle()': {
+		'single child'() {
+			const parent = { children: List<any>(), append(): Handle { return; } };
+			const child = {
+				parent,
+				own(handle: Handle): Handle { return handle; },
+				destroy(): Promise<boolean> {
+					return Promise.resolve(true);
+				},
+				render(): VNode { return; },
+				tagName: ''
+			};
+			child.parent.children = child.parent.children.push(child);
+			const handle = getRemoveHandle(parent, child);
+			assert.strictEqual(child.parent, parent);
+			assert.isTrue(child.parent.children.includes(child));
+			handle.destroy();
+			assert.isUndefined(child.parent);
+			handle.destroy();
+		},
+
+		'child array'() {
+			const parent = { children: List<any>(), append(): Handle { return; } };
+			const child = {
+				parent,
+				own(handle: Handle): Handle { return handle; },
+				destroy(): Promise<boolean> {
+					return Promise.resolve(true);
+				},
+				render(): VNode { return; },
+				tagName: ''
+			};
+			child.parent.children = child.parent.children.push(child);
+			const handle = getRemoveHandle(parent, [ child ]);
+			assert.strictEqual(child.parent, parent);
+			assert.isTrue(child.parent.children.includes(child));
+			handle.destroy();
+			assert.isUndefined(child.parent);
+			handle.destroy();
+		},
+
+		'child map'() {
+			const parent = { children: List<any>(), append(): Handle { return; } };
+			const child = {
+				parent,
+				own(handle: Handle): Handle { return handle; },
+				destroy(): Promise<boolean> {
+					return Promise.resolve(true);
+				},
+				render(): VNode { return; },
+				tagName: ''
+			};
+			child.parent.children = child.parent.children.push(child);
+			const handle = getRemoveHandle(parent, { child });
+			assert.strictEqual(child.parent, parent);
+			assert.isTrue(child.parent.children.includes(child));
+			handle.destroy();
+			assert.isUndefined(child.parent);
+			handle.destroy();
+		}
+	},
+
+	'isList()'() {
+		const list = List();
+		const arr: any[] = [];
+		assert.isTrue(isList(list));
+		assert.isFalse(isList(arr));
+	},
+
+	'isChild()'() {
+		const child = createRenderable();
+		const notChild = {};
+		assert.isTrue(isChild(child));
+		assert.isFalse(isChild(notChild));
+	},
+
 	'stringToValue()'() {
 		assert.isUndefined(stringToValue(''), 'emtpy string returns undefined');
 		assert.deepEqual(stringToValue('["foo",true,2]'), [ 'foo', true, 2 ]);
@@ -198,6 +322,7 @@ registerSuite({
 		assert.isTrue(stringToValue('true'));
 		assert.isFalse(stringToValue('false'));
 	},
+
 	'valueToString()'() {
 		assert.strictEqual(valueToString({ foo: 2 }), '{"foo":2}');
 		assert.strictEqual(valueToString([ 1, 2, 3 ]), '[1,2,3]');
@@ -210,20 +335,5 @@ registerSuite({
 		assert.strictEqual(valueToString(0), '0');
 		assert.strictEqual(valueToString(true), 'true');
 		assert.strictEqual(valueToString(false), 'false');
-	},
-	'isList()'() {
-		const list = List();
-		const arr: any[] = [];
-		assert.isTrue(isList(list));
-		assert.isFalse(isList(arr));
-	},
-	'isChild()'() {
-		const child = createRenderable();
-		const notChild = {};
-		assert.isTrue(isChild(child));
-		assert.isFalse(isChild(notChild));
-	},
-	'getRemoveHandle()': {
-		/* TODO: create tests */
 	}
 });
